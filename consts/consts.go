ca063c01175f1e87e25c1e944522213055d7dc91
uchizy <ugbechieedmund073@gmail.com>
2024-05-23 03:03:38 +0100

    complete

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = "UchizyChain"
+	HRP = "JanChain"
 	// TODO: choose a name for your hyperchain
-	Name = "UchizyHyperChain"
+	Name = "JanHyperChain"
 	// TODO: choose a token symbol
-	Symbol = "UHC"
+	Symbol = "JHC"
 )
 
 var ID ids.ID
